89d5ad135be8ab1dcbbba056bac2b9088d068397
Thomás Freire da Silva Reis <thomas.fsr@gmail.com>
2025-12-09 18:52:47 -0300

    ok

--- internal/domain/entities/message/message.zig
+++ internal/domain/entities/message/message.zig
@@ -1,10 +1,21 @@
 const Message = struct {
-    ID : ID,
-    UserID : ID,
-    Role : Role,
+    ID : u32,
+    UserID : u32,
+    Role : enum{user, assistant},
     Content : Content,
+    tokens : u32
 };
 
-const ID = u32;
-const Role =enum{user, assistant}; 
-const Content = []u8;
+const Content = struct {
+    content : []const u8,
+    fn init(content: []const u8) @This() {
+        return .{.content = content};
+    }
+    fn update(self: @This(), new_content: []const u8) void {
+        self.content = new_content;
+    }
+    fn get_value(self: @This()) []const u8 {
+        return self.content;
+    }
+};
+
