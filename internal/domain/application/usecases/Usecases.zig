1f25c190f89d218bd62a0a7da9a9ce7a2c55b07b
Thomás Freire da Silva Reis <thomas.fsr@gmail.com>
2025-12-10 17:10:43 -0300

    ok

--- internal/domain/application/usecases/Usecases.zig
+++ internal/domain/application/usecases/Usecases.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
 const UserRepository = struct {
-    save_user : *const fn (ptr: *UserRepository) void
+    save_user : *const fn (ptr: *UserRepository) void,
     find_by_ID : *const fn (ptr: *UserRepository) void
 };
